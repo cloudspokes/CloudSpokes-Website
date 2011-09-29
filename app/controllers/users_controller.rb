401167d6cbfa86a57093864b0969f4195d5744b0
Jeff Douglas <jeff@jeffdouglas.com>
2011-09-29 11:26:28 -0400

    oauth changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   before_filter :load_user, :except => [:index, :new]
   
   def index
-    @users = User.all
+    # @users = User.all
   end
   
   def show
