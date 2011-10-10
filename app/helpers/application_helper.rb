e08acfb70669e544616e71b5f6d14a9391e5b418
Jeff Douglas <jeff@jeffdouglas.com>
2011-10-10 11:05:15 -0400

    Changed salesforce credentials

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   
   config = YAML.load_file(File.join(::Rails.root, 'config', 'databasedotcom.yml'))
   client = Databasedotcom::Client.new(config)
-  ENV['access_token'] = client.authenticate :username => ENV['sfdc_username'], :password => ENV['sfdc_password']
+  ENV['access_token'] = client.authenticate :username => 'mess@cloudspokes.com.dev091', :password => '74H3yd2NZW'
   
   def s3_image(path, options = {})
     image_tag("http://cloudspokes.s3.amazonaws.com/#{path}", options)
