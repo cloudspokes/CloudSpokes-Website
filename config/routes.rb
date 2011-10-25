6195719a43f10681b74985f64b06c1804388b810
Kishore Annapureddy <kishore@kishore-laptop.(none)>
2011-10-25 07:49:11 +0530

    CloudSpokes challenges functionality added

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Cloudspokes::Application.routes.draw do
   root to: 'content#show', id: 'home'
+  match 'leaderboard', :to => 'challenges#leaderboard'
   match "/:id", to: "content#show", as: "content"
   
   get 'challenges/index'
+  match 'challenges/:id', :to => 'challenges#detail'
+  match 'challenges/:id/registrants', :to => 'challenges#registrants'
   get 'sessions/login'
   get 'sessions/sfdcauth'
   match '/auth/:provider/callback', :to => 'sessions#create'
--- lib/challenges.rb
+++ lib/challenges.rb
@@ -7,7 +7,32 @@ class Challenges
   headers 'Authorization' => "OAuth #{ENV['access_token']}"
 
   def self.get_challenges()   
-    get('https://na12.salesforce.com/services/apexrest/v.9/challenges')
-  end 
+    get('https://na12.salesforce.com/services/apexrest/v.9/challenges?fields=Id,Name,Description__c,Top_Prize__c,Registered_Members__c,End_Date__c')
+  end
   
+  def self.get_challenge_detail(id)
+    get('https://na12.salesforce.com/services/apexrest/v.9/challenges/'+id+'?fields=Id,Name,Description__c,Additional_Info__c,Comments__c,End_date__c,License__c,Requirements__c,Status__c,Submission_Details__c,Top_Prize__c,Winner_Announced__c,Registered_Members__c')
+  end
+  
+  def self.get_categories(id)
+    catnames = []
+    cats = get('https://na12.salesforce.com/services/apexrest/v.9/challenges/'+id+'/categories')
+    cats.each do |cat|
+        catobj = get('https://na12.salesforce.com'+cat['attributes']['url']+'?fields=Category__r.Name')
+        catnames.push(catobj['Category__r']['Name'])
+    end
+    return catnames
+  end
+  
+  def self.get_prizes(id)
+    get('https://na12.salesforce.com/services/apexrest/v.9/challenges/'+id+'/prizes?fields=Prize__c,Place__c')
+  end
+  
+  def self.get_registrants(id)
+    get('https://na12.salesforce.com/services/apexrest/v.9/participants?challengeid='+id+'&fields=Member__r.Profile_Pic__c,Member__r.Name,Member__r.Total_Wins__c')
+  end
+  
+  def self.get_leaderboard(from_date, page_num)
+    get('https://na12.salesforce.com/services/apexrest/v.9/leaderboard?pageNum='+page_num.to_s+'&dateFormat='+from_date.to_s)
+  end
 end
